8031627bb8815d201ab991b6a3da6f7306e8b3d5
Ashomondi <adhiamboa745@gmail.com>
2026-08-06 12:24:38 +0300

    struct budget for the user

--- backend/models/budget.go
+++ backend/models/budget.go
@@ -3,11 +3,10 @@ package models
 import "time"
 
 type Budget struct {
-	ID uint `gorm:"primaryKey" json:"id"`
-	UserID  uint `gorm:"not null" json:"user_id"`
-	Category string `gorm:"not null" json:"category"`
-	Amount float64 `gorm:"not null" json:"amount"`
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	UserID    uint      `gorm:"not null" json:"user_id"`
+	Category  string    `gorm:"not null" json:"category"`
+	Amount    float64   `gorm:"not null" json:"amount"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-
 }
