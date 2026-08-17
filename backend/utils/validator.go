01d3305205d71fe24fd964af01f98ad7535190bb
Ashomondi <adhiamboa745@gmail.com>
2026-08-17 16:52:36 +0300

    added a function that checks oiif the email and the password follow the creteria inputed

---
+++ backend/utils/validator.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"net/mail"
+	"regexp"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func IsValidEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func IsStrongPassword(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+	hasLetter := regexp.MustCompile(`[a-zA-Z]`).MatchString(password)
+	hasNumber := regexp.MustCompile(`[0-9]`).MatchString(password)
+
+	return hasLetter && hasNumber
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
